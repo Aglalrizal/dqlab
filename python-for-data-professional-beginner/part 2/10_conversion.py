96fa77c8d51c966c1e2f2a88b77db3f12775e005
Aglalrizal <rizalaglalfaoji017@gmail.com>
2026-07-06 22:10:19 +0700

    [python-for-data-professional-beginner] part 2  - collection manipulation 2

---
+++ python-for-data-professional-beginner/part 2/10_conversion.py
@@ -0,0 +1,22 @@
+# Tuple
+print(">>> Tuple")
+tuple_menu = ('Gado-gado','Ayam Goreng','Rendang','Ketoprak')
+jumlah_menu = len(tuple_menu)
+print(jumlah_menu)
+# Output: 4
+# List
+print(">>> List")
+list_menu = ['Gado-gado','Ayam Goreng','Rendang','Ketoprak']
+jumlah_menu = len(list_menu)
+print(jumlah_menu)
+# Output: 4
+# Konversi tipe data
+print(">>> Konversi tipe data")
+list_buah = ['Apel', 'Apel', 'Jeruk', 'Markisa', 'Jeruk', 'Markisa', 'Apel']
+set_buah = set(list_buah)
+print(set_buah)
+# Output: {'Markisa', 'Jeruk', 'Apel'}
+list_buah = list(set_buah)
+list_buah.sort()
+print(list_buah)
+# Output: {'Apel', 'Jeruk', 'Markisa'}
