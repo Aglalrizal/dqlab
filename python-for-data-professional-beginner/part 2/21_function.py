ccb193c0d014d0859fadc6ba939ef75a1919ae68
Aglalrizal <rizalaglalfaoji017@gmail.com>
2026-07-06 22:40:17 +0700

    [python-for-data-professional-beginner] part 2 - function

---
+++ python-for-data-professional-beginner/part 2/21_function.py
@@ -0,0 +1,9 @@
+# Definisikan fungsi
+def contoh_fungsi():
+    print("Halo Dunia")
+    print("Aku sedang belajar bahasa Python")
+# Panggil fungsi yang telah didefinisikan
+contoh_fungsi()
+# Output: 
+# Halo Dunia
+# Aku sedang belajar bahasa Python
