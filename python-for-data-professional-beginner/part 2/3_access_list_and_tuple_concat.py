041f4648ef53a7c0aa976985b4378fdb8f97dd22
Aglalrizal <rizalaglalfaoji017@gmail.com>
2026-07-06 14:05:00 +0700

    [python-for-data-professional-beginner] part 2 - access list and tuple

---
+++ python-for-data-professional-beginner/part 2/3_access_list_and_tuple_concat.py
@@ -0,0 +1,7 @@
+list_makanan = ['Gado-gado', 'Ayam Goreng', 'Rendang']
+list_minuman = ['Es Teh', 'Es Jeruk', 'Es Campur']
+list_menu = list_makanan + list_minuman
+print(list_menu)
+
+# output:
+# ['Gado-gado', 'Ayam Goreng', 'Rendang', 'Es Teh', 'Es Jeruk', 'Es Campur']
